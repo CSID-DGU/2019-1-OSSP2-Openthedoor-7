a27f386042105db6bde153a4ef7e3b5de207e8e2
miranlee <48276682+rannan09@users.noreply.github.com>
2019-05-05 09:22:30 +0900

    Add files via upload

--- FaceTracker/src/exe/face_tracker.cc
+++ FaceTracker/src/exe/face_tracker.cc
@@ -39,11 +39,76 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <FaceTracker/Tracker.h>
 #include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <GL/freeglut.h>
+#include <GL/glut.h>
+#include <GL/glu.h>
 #include <iostream>
+
+using namespace cv;
+using namespace std;
+
+GLuint texture_background;
+//
+void oglLine (double sx, double sy, double sz, double ex, double ey, double ez)
+{	
+	glBegin(GL_LINES);
+		glVertex3d(sx, sy, sz);
+		glVertex3d(ex, ey, ez);
+	glEnd();
+}
+
+void oglBox (double dx, double dy, double dz) 
+{
+	glBegin(GL_QUADS);
+		glNormal3d(0., 0., 1.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d( dx,  dy,  dz);	
+		glTexCoord2f(100.0f,   0.0f); glVertex3d( dx,   0,  dz);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d(  0,   0,  dz);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d(  0,  dy,  dz);
+
+		glNormal3d(0., 0., -1.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d(  0,  dy,   0);
+		glTexCoord2f(100.0f,   0.0f); glVertex3d(  0,   0,   0);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d( dx,   0,   0);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d( dx,  dy,   0);
+
+		glNormal3d(1., 0., 0.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d( dx,  dy,  dz);
+		glTexCoord2f(100.0f,   0.0f); glVertex3d( dx,  dy,   0);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d( dx,   0,   0);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d( dx,   0,  dz);
+
+		glNormal3d(-1., 0., 0.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d(  0,  dy,  dz);
+		glTexCoord2f(100.0f,   0.0f); glVertex3d(  0,  dy,   0);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d(  0,   0,   0);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d(  0,   0,  dz);
+
+		glNormal3d(0., 1., 0.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d(  0,  dy,  dz);
+		glTexCoord2f(100.0f,   0.0f); glVertex3d( dx,  dy,  dz);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d( dx,  dy,   0);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d(  0,  dy,   0);
+
+		glNormal3d(0., -1., 0.);
+		glTexCoord2f(  0.0f,   0.0f); glVertex3d(  0,   0,  dz);
+		glTexCoord2f(100.0f,   0.0f); glVertex3d( dx,   0,  dz);
+		glTexCoord2f(100.0f, 100.0f); glVertex3d( dx,   0,   0);
+		glTexCoord2f(  0.0f, 100.0f); glVertex3d(  0,   0,   0);
+	glEnd();
+}
+
 //=============================================================================
 void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
-{
-  int i,n = shape.rows/2; cv::Point p1,p2; cv::Scalar c;
+{ //Draw(im,model._shape,con,tri,model._clm._visi[idx]);
+//image:출력될 곳, shape:현재 이미지, visi:Visibility for each view(벡터)
+  int i,n = shape.rows/2; 
+  cv::Point p1,p2; 
+  cv::Scalar c;
+  cv::Point pts[6];
+  int pts_count=0;
 
   //draw triangulation
   c = CV_RGB(0,0,0); //검정선
@@ -70,8 +135,7 @@ void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
   //draw connections
   c = CV_RGB(0,0,255);//파란선
   for(i = 0; i < con.cols; i++){
-    if(visi.at<int>(con.at<int>(0,i),0) == 0 ||
-       visi.at<int>(con.at<int>(1,i),0) == 0)continue;
+    if(visi.at<int>(con.at<int>(0,i),0) == 0 || visi.at<int>(con.at<int>(1,i),0) == 0)continue;
     p1 = cv::Point(shape.at<double>(con.at<int>(0,i),0),
 		   shape.at<double>(con.at<int>(0,i)+n,0));
     p2 = cv::Point(shape.at<double>(con.at<int>(1,i),0),
@@ -82,13 +146,26 @@ void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
   for(i = 0; i < n; i++){    
     if(visi.at<int>(i,0) == 0)
     continue;
+//
     p1 = cv::Point(shape.at<double>(i,0),shape.at<double>(i+n,0));
     c = CV_RGB(255,0,0); //빨간 원
     cv::circle(image,p1,2,c);
-  }return;
+    //cv::putText(image,std::to_string(i),p1,CV_FONT_HERSHEY_COMPLEX,0.5,c); //인덱싱
+    if(i==60||i==61||i==62||i==63||i==64||i==65)
+    {
+      pts[pts_count++]=p1;
+      c=CV_RGB(0,0,0);
+      cv::putText(image,std::to_string(i),p1,CV_FONT_HERSHEY_COMPLEX,0.5,c);
+    }
+  }
+  //영역의 크기 구하기
+  c = CV_RGB(255,0,0);
+  cv::fillConvexPoly(image,pts,6,c);
+ 
+  return;
 }
 //=============================================================================
-int parse_cmd(int argc, const char** argv,
+int parse_cmd(int argc, char** argv,
 	      char* ftFile,char* conFile,char* triFile,
 	      bool &fcheck,double &scale,int &fpd)
 {
@@ -159,7 +236,126 @@ int parse_cmd(int argc, const char** argv,
   return 0;
 }
 //=============================================================================
-int main(int argc, const char** argv)
+//=============================================================================
+GLuint MatToTexture(Mat &image) //openCV형 변수를 openGL형 변수로 바꾸는 함수
+{
+    if (image.empty())  return -1;
+ 
+    //OpenGL 텍스처 생성
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+ 
+    //텍스처 ID를 바인딩 -  사용할 텍스처 차원을 지정해준다.
+    glBindTexture(GL_TEXTURE_2D, textureID); 
+ 
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+ 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows,
+        0, GL_RGB, GL_UNSIGNED_BYTE, image.ptr());
+  std::cout<<"m to t 완료"<<endl;
+    return textureID;
+}
+
+void display(Mat &image){
+
+//   // glEnable(GL_TEXTURE_2D);
+//   // glBindTexture(GL_TEXTURE_2D, texture_background);
+
+//   glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //glClear에서 컬러 버퍼 지운 후 윈도우를 채울 색을 지정, 검은색
+//   glClear(GL_COLOR_BUFFER_BIT);         //컬러 버퍼를 지운다.
+//   glLoadIdentity();
+
+//   glBegin(GL_QUADS); //4점이 하나의 사각형을 구성한다. 반시계 방향으로 4점의 vertex를 지정해줘야 한다.
+//   glVertex2f(-0.5f, -0.5f);    // x, y
+//   glVertex2f(0.5f, -0.5f);
+//   glVertex2f(0.5f, 0.5f);
+//   glVertex2f(-0.5f, 0.5f);
+//   glEnd();
+
+//   glutSwapBuffers();
+
+// cout<<"디스플레이 함수 완료"<<endl;
+
+  int width = 640;
+	int height = 480;
+	double fovy = 45.;
+
+  glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+  glViewport(0, 0, width, height);
+	gluPerspective(fovy, (double)width/height, 0.1, 1000.);
+
+	// 그리기 위한 Render 모드로 변경한다.
+	glRenderMode(GL_RENDER);
+
+	// Modelview Matrix를 선택하고 초기화한다. 
+	glMatrixMode(GL_MODELVIEW);    
+	glLoadIdentity();
+
+	// Screen 과 Depth Buffer를 지운다.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	gluLookAt( -3, 0, 0, 0, 0, 0, 0,0,1 );
+
+	double x = 0;
+	double y = 0;
+	double z = 0;
+	static double yaw = 30;
+	double pitch = 30;
+	double roll = 0;
+
+	yaw += 3;
+
+	// x, y, z 축으로 좌표계를 이동하고 회전한다.
+	glTranslated (x, y, z);
+	glRotated (yaw  , 0, 0, 1);	// z-축을 회전축으로 yaw 만큼 회전
+	glRotated (pitch, 0, 1, 0);	// y-축을 회전축으로 pitch 만큼 회전
+	glRotated (roll , 1, 0, 0);	// x-축을 회전축으로 roll 만큼 회전
+
+	// x, y, z 좌표 축을 그린다.
+	glDisable(GL_LIGHTING);
+	glLineWidth(3);
+	glColor3d(1, 0, 0);
+	oglLine(0,0,0, 1,0,0);
+	glColor3d(0, 1, 0);
+	oglLine(0,0,0, 0,1,0);
+	glColor3d(0, 0, 1);
+	oglLine(0,0,0, 0,0,1);
+	glEnable(GL_LIGHTING);
+
+	// 박스를 그린다.
+	glColor3d(0.5, 0.5, 0.5);
+	oglBox(0.5, 0.5, 0.5);
+  
+  glEnd();
+	glFinish();
+
+  IplImage imgTmp=image;
+  IplImage *img_bg=cvCloneImage(&imgTmp);
+
+	IplImage *img_gl = cvCreateImage (cvSize(width, height), IPL_DEPTH_8U, 3);
+	IplImage *mask   = cvCreateImage (cvSize(width, height), IPL_DEPTH_8U, 1);
+	
+  glReadPixels (0, 0, width, height, GL_BGR_EXT, GL_UNSIGNED_BYTE, img_gl->imageData );
+
+  cvCvtColor (img_gl, mask, CV_RGB2GRAY);
+  cvThreshold(mask, mask, 1, 255, CV_THRESH_BINARY);
+
+// OpenCV로 읽은 이미지에다가 마스크를 지정하여 OpenGL 물체가 있는 부분만 복사한다.
+  cvCopy (img_gl, img_bg, mask);
+  image=cvarrToMat(cvCloneImage(img_bg));
+
+  //cvShowImage ("img", img_bg);
+
+   cvReleaseImage (&mask);
+   cvReleaseImage (&img_gl);
+   cvReleaseImage (&img_bg);
+}
+
+//--------------------------------------------------------------------------
+int main(int argc, char** argv)
 {
   //parse command line arguments
   char ftFile[256],conFile[256],triFile[256];
@@ -167,10 +363,19 @@ int main(int argc, const char** argv)
   double scale = 1; 
   int fpd = -1;
   bool show = true;
+
+  glutInit(&argc,argv); 
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(400, 400);
+  glutInitWindowPosition(0, 0);
+
+  glutCreateWindow("OpenGL");
+  glutHideWindow();
+
   if(parse_cmd(argc,argv,ftFile,conFile,triFile,fcheck,scale,fpd)<0) //추가 옵션 명령어 처리
   return 0;
   //set other tracking parameters
-
+ 
   std::vector<int> wSize1(1);
    wSize1[0] = 7;
   std::vector<int> wSize2(3);
@@ -193,8 +398,9 @@ int main(int argc, const char** argv)
    std::cout<<"please check your camera!"<<std::endl;
    return -1;
    }
-  int64 t1,t0 = cvGetTickCount(); int fnum=0;
-  cvNamedWindow("Face Tracker",1);
+  int64 t1,t0 = cvGetTickCount(); //framerate 출력을 위한 변수
+   int fnum=0;
+  cvNamedWindow("Face Tracker",1); // 윈도우 창 생성 
   std::cout << "Hot keys: "        << std::endl
 	    << "\t ESC - quit"     << std::endl
 	    << "\t d   - Redetect" << std::endl;
@@ -203,21 +409,31 @@ int main(int argc, const char** argv)
   bool failed = true;
   while(1){ 
     //grab image, resize and flip
+    camera.read(frame); //frame 변수에 카메라 읽어온 값을 넣음
+    //glutCreateWindow("OpenGL");
 
+    if(scale == 1){
+    im = frame; 
+    }
+    else {
+    cv::resize(frame,im,cv::Size(scale*frame.cols,scale*frame.rows));
+    }
+    cv::flip(im,im,1);
+    cv::cvtColor(im,gray,CV_BGR2GRAY);
 
-    camera.read(frame);
-    if(scale == 1)im = frame; 
-    else cv::resize(frame,im,cv::Size(scale*frame.cols,scale*frame.rows));
-    cv::flip(im,im,1); cv::cvtColor(im,gray,CV_BGR2GRAY);
-
-    //track this image
-    std::vector<int> wSize; if(failed)wSize = wSize2; else wSize = wSize1; 
+    //이미지 track
+    std::vector<int> wSize; 
+    if(failed)wSize = wSize2;
+     else wSize = wSize1; 
     if(model.Track(gray,wSize,fpd,nIter,clamp,fTol,fcheck) == 0){
       int idx = model._clm.GetViewIdx();
        failed = false;
-      Draw(im,model._shape,con,tri,model._clm._visi[idx]); 
-
-    }else{
+      Draw(im,model._shape,con,tri,model._clm._visi[idx]); //im에 선과 점을 그림
+      //texture_background=MatToTexture(im); 
+      //std::cout<<"main m to t 완료"<<endl;
+      display(im);
+    }
+    else{
       if(show){
         cv::Mat R(im,cvRect(0,0,150,50)); 
         R = cv::Scalar(0,0,255);
@@ -238,12 +454,15 @@ int main(int argc, const char** argv)
       cv::putText(im,text,cv::Point(10,20),CV_FONT_HERSHEY_SIMPLEX,0.5,CV_RGB(255,255,255));
     }
     //show image and check for user input
-    imshow("Face Tracker",im); //창을 화면에 띄움
-    int c = cvWaitKey(10);
+
+    imshow("Face Tracker",im); //생성된 창에 카메라 화면 띄움
+    int c = cvWaitKey(10); //사용자의 입력을 기다림 
     if(c == 27)
     break; 
     else if(char(c) == 'd')
-    model.FrameReset(); //d가 입력되면 재인식
+    model.FrameReset(); //d가 입력되면 프레임 재인식
+  
+    //glutMainLoop();
   }
   return 0;
 }
