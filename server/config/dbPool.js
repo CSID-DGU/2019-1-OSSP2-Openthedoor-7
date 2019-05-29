de417b54d84c248d068d18aa29769d237011d2fb
hyex <48546343+hyex@users.noreply.github.com>
2019-05-29 18:51:19 +0900

    Delete dbPool.js

--- server/config/dbPool.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('promise-mysql');
-const config = {
-    host: 'openthedoor.carwy5e8omya.ap-northeast-2.rds.amazonaws.com',
-    port: 3306,
-    user: 'openthedoor',
-    password: '2019ossp7',
-    database: 'openthedoor',
-}
-module.exports = mysql.createPool(config);
